6f46ca3b8e3b604054350e4b0bfdd4d03f5e18c6
Alexander Bayagin <a.bredogen@gmail.com>
2022-05-28 17:28:45 +0500

    Fix YAML/JSON provider when file is empty

--- src/main/kotlin/com/bredogen/projectenv/providers/files/YamlFileProvider.kt
+++ src/main/kotlin/com/bredogen/projectenv/providers/files/YamlFileProvider.kt
@@ -23,12 +23,16 @@ class YamlFileProvider(private val params : Map<String, String>) : EnvFileProvid
     override fun getEnvValues(): LinkedHashMap<String, String> {
         val path = params["path"] ?: throw EnvSourceException("No valid path to json/yaml file")
 
-        val result: LinkedHashMap<String, String>
+        var result: LinkedHashMap<String, String>?
         try {
             result = Yaml().load(Files.readString(Paths.get(path)))
         } catch (ex: IOException) {
             throw EnvSourceException(ex)
         }
+        if (result == null) {
+            result = linkedMapOf()
+        }
+
         return result
     }
 }
